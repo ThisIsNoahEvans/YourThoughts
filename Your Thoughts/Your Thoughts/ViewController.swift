be97a989498c6f05ccc8994b201653c6a2a60308
Noah Evans <noah@itsnoahevans.co.uk>
2020-08-09 11:33:28 +0100

    Blank project

---
+++ Your Thoughts/Your Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Your Thoughts
+//
+//  Created by Noah Evans on 09/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
